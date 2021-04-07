7ea8d5853c2a0e1d0f58851851ce233aaee18afb
steven <steven.h@live.com>
2021-04-06 17:05:53 -0700

    add todo.js and add content div to index

---
+++ src/todo.js
@@ -0,0 +1,12 @@
+
+
+
+
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+  }
+}
